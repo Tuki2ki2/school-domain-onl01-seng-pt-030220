222fb080106a241d60653eeb77cfcda41d6f1e0a
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-18 17:44:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School 
+  attr_reader :grade, :name, :roster, :sort
+  attr_accessor :school_name
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {} 
+
+    end
+    
+    def add_student(name, grade)
+      if @roster.include?(grade)
+        @roster[grade] << name 
+      else
+      @roster[grade] = [] 
+      @roster[grade] << name 
+    end
+    end
+    
+    def grade(grade)
+      roster[grade]
+    end
+    
+    def sort
+      roster.map do|key, value| 
+      value.sort
+    end
+    end
+      
+  
+  
+end
